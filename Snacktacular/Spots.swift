af4013e8a8b0bd1147b7dbe03dae76d992f2c21b
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-03-31 17:04:15 -0400

    Section 9.3

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,22 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by BC Swift Student Loan 1 on 3/30/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import CoreLocation//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by BC Swift Student Loan 1 on 3/30/19.
+//  Copyright © 2019 Richard Li. All rights reserved.
+//
+
+import Foundation
+
+class Spots{
+    var spotArray = [Spot]()
+}

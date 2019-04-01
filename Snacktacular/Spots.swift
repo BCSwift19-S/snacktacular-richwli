c69acf46b3ac54104a9d7cae10632a59100bff26
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-04-01 00:27:24 -0400

    Section 9.6

--- Snacktacular/Spots.swift
+++ Snacktacular/Spots.swift
@@ -7,7 +7,8 @@
 //
 
 import Foundation
-import CoreLocation//
+import CoreLocation
+import Firebase
 //  Spots.swift
 //  Snacktacular
 //
@@ -15,8 +16,28 @@ import CoreLocation//
 //  Copyright © 2019 Richard Li. All rights reserved.
 //
 
-import Foundation
-
 class Spots{
     var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("*** ERROR: adding the snapshot listener \(error!.localizedDescription)")
+                return completed()
+            }
+            self.spotArray = []
+            // there are querySnapshot!.documents in the spots snapshot
+            for document in querySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            completed()
+        }
+    }
 }
